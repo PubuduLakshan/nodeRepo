5002920ac2365ddce65267777d954a50b9b8777d
3LOGIXS\pubudu_k <pubudu.kulasinghe@gmail.com>
2018-11-19 11:05:47 +0530

    put middle commit

--- index.js
+++ index.js
@@ -10,12 +10,30 @@ const courses = [
     {id:4,title:'Sinhala'},
 ];
 
+function validateBody(course){
+    const schema = {
+        name : Joi.string().min(3).required()
+    }
+    return Joi.validate(course,schema);
+}
+
+app.put('/api/course/:id',(req,res)=>{
+    const course = courses.find(c=>{c.id===parseInt(req.params.id)});
+    if(!course){res.status(404).send('Page Not Found')}
+    const {error} = validateBody(req.body);
+    if(error){res.status(400).send(error.details[0].message); return;}
+
+    course.name = req.body.name;
+    res.send(course);
+
+})
+
 app.post('/api/courses',(req,res)=>{
     /*if(!req.body.name || req.body.name.length<3){
         res.status(400).send("name doesn't exist or too short");
         return;
     }*/
-    const schema = {
+    /*const schema = {
         name: Joi.string().min(3).required()
     }
 
@@ -23,7 +41,10 @@ app.post('/api/courses',(req,res)=>{
     if(result.error){
         res.status(400).send(result.error.details[0].message);
         return;     
-    }
+    }*/
+    const {error} = validateBody(req.body);
+    if(error){res.status(400).send(error.details[0].message); return;}
+
     const course = {
         id : courses.length + 1,
         name : req.body.name
